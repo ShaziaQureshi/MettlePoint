692eda8e722b295dd24e5be2a0bba37b3e0fe178
ShaziaQureshi <saziaakram123@gmail.com>
2022-01-12 12:09:49 +0530

    2 files modified, 1 added

--- src/Personalized.js
+++ src/Personalized.js
@@ -1,17 +1,55 @@
 import React,{Component} from "react"
 import ReactDOM from "react-dom"
+import {init} from '@emailjs/browser'
+emailjs.init("user_SG2lXtweFNiLdVeQjzP5V");
+import emailjs from 'emailjs-com';
+import emailkey from "./emailkey"
 const persoData="We all are different in our own ways and so is our healing journey. That is the reason we are also offereing personalized Help for every indivaual. If the above content intrigued you and you are at this page, make sure to provide your details below so we can reach out for you."
 
 class Personalized extends Component{
-constructor(){
-  super()
-  this.handleOnClick=this.handleOnClick.bind()
+constructor(props){
+  super(props)
+  this.state={
+    name:"",
+    email:"",
+    message:"",
+    phone:""
+  };
+  this.handleOnClick=this.handleOnClick.bind(this)
+  this.handleOnSubmit=this.handleOnSubmit.bind(this)
 }
 handleOnClick(event){
-const value=event.target.value
-console.log(value)
-if(name=="Username")
-  console.log("Name")
+const data=event.target.value
+const namevar=event.target.name
+//console.log(data)
+if(namevar=="Username")
+  {
+    this.setState({name:data})
+  }
+  else if(namevar=="email")
+    this.setState({email:data})
+  else if(namevar=="message")
+    this.setState({message:data})
+  else if(namevar=="phone")
+    this.setState({phone:data})
+}
+handleOnSubmit(event){
+event.preventDefault()
+console.log(emailkey.USER_ID)
+ var templateParams = {
+    to_name: 'xyz',
+    from_name: 'abc',
+    message_html: 'Please Find out the attached file'
+  }
+emailjs.send('service_e1y1fun', 'template_cczqded', templateParams)
+.then(function(response) {
+  console.log('SUCCESS!', response.status, response.text);
+}, function(error) {
+  console.log('FAILED...', error);
+});
+//emailjs.send("service_e1y1fun","template_cczqded");
+//emailjs.send('gmail',emailkey.TEMPLATE_ID,event.target,emailkey.USER_ID)
+
 
 }
   render(){
@@ -28,25 +66,32 @@ if(name=="Username")
                     <input className="form-data"
                     placeholder="Enter Your Name"
                     name="Username"
-                    onClick={this.handleOnClick}
+                    onChange={this.handleOnClick}
                     type="text"></input><br/>
 
                     <lable className="form--lable">E-Mail</lable><br/>
                     <input className="form-data"
                     placeholder="Enter Your Email Address"
+                    name="email"
+                    onChange={this.handleOnClick}
                     type="text"></input><br/>
 
                     <lable className="form--lable">Message</lable><br/>
                     <input className="form-data"
                     placeholder="Thought?"
+                    name="message"
+                    onChange={this.handleOnClick}
                     type="textArea"></input><br/>
 
                     <lable className="form--lable">Phone Number</lable><br/>
                     <input className="form-data"
                     placeholder="Enter Your Phone Number"
+                    name="phone"
+                    onChange={this.handleOnClick}
                     type="text"></input><br/>
 
-                    <button>SUBMIT</button>
+                    <button
+                    onClick={this.handleOnSubmit}>SUBMIT</button>
                 </form>
             </div>
            </div>
---
+++ src/emailkey.js
@@ -0,0 +1,5 @@
+export default{
+  USER_ID:'user_SG2lXtweFNiLdVeQjzP5V',
+  TEMPLATE_ID:'template_cczqded'
+
+}
