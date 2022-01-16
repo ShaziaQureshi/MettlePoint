b7eb6d5f7eef7259ef30a9a5c90446ad7c11a243
ShaziaQureshi <saziaakram123@gmail.com>
2022-01-17 00:22:01 +0530

    2 files modified, 3 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./style.css";
 import Navigation from "./Navigation"
+import MainPage from "./MainPage"
 import Personalized from "./Personalized"
 export default function App() {
   return (
     <div>
       <Navigation/>
-      {/* <Personalized /> */}
+      <MainPage />
+      <Personalized />
     </div>
   );
 }
---
+++ src/MainPage.js
@@ -0,0 +1,23 @@
+import React from "react"
+import ReactDOM from "react-dom"
+function MainPage(){
+  return(
+    <main>
+      <div>
+      <img src="https://static01.nyt.com/images/2019/03/14/smarter-living/yolb-november-speak-publi-slide-H575/yolb-november-speak-publi-slide-H575-jumbo.jpg" width="100%"/>
+      <span className="image--caption">
+        Remove Toxic Insecurities and say Hello to Confidence.
+        </span>
+        <div className="main--buttons">
+        <button className="ImageButton">CONTINUE</button>
+        <button className="ImageButton">PERSONALIZED HELP</button>
+        </div>
+        
+
+      </div>
+      </main>
+  )
+}
+export default MainPage
+
+// https://static01.nyt.com/images/2018/11/01/smarter-living/yolb-november-speak-publi-promo/yolb-november-speak-publi-slide-H575-blog480.jpg
